8cca5ac9fc0e570e85be1f00c2b3a265bbf9234d
hassanpanah <hassanpanah_m@ut.ac.ir>
2023-10-27 22:36:49 +0330

    A little bit complex

--- numpyfornumpy.py
+++ numpyfornumpy.py
@@ -1,5 +1,7 @@
 import numpy as np
-ar1=np.array([1,2,4])
-ar2=np.array([5,3,4])
-j=np.concatenate((ar1,ar2))
+ar1=np.array([[1,2],[5,6]])
+ar2=np.array([[5,3],[6,9]])
+j=np.concatenate((ar1,ar2),axis=1)
+print(ar1)
+print(ar2)
 print(j)
